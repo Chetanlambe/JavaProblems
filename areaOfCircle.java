71b9f92c6a92b8e475c1d80fdce38318789c0429
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-06 17:04:34 +0530

    Update areaOfCircle.java

--- areaOfCircle.java
+++ areaOfCircle.java
@@ -5,6 +5,7 @@ public class areaOfCircle {
         double r; // r = radius
         double a; // area of circle
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius number");
         r = sc.nextFloat();
         a = 2 * 3.14 * r * r;
         System.out.println("Area of circle "+ a);
