7da2d8f7ac85343c12985cbf8fc5ecc6f9186510
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-06 16:52:13 +0530

    Area of circle
    find the area of the circle

---
+++ areaOfCircle.java
@@ -0,0 +1,13 @@
+// find the area of circle
+import java.util.Scanner;
+public class areaOfCircle {
+    public static void main(String[] args) {
+        double r; // r = radius
+        double a; // area of circle
+        Scanner sc = new Scanner(System.in);
+        r = sc.nextFloat();
+        a = 2 * 3.14 * r * r;
+        System.out.println("Area of circle "+ a);
+
+    }
+}
