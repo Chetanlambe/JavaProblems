61360227eb872c3a2b0ef716b4df05711a2d9821
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-07 19:03:28 +0530

    areaPerimeterGreater
    calculate the area and perimeter rectangle and which is greater

---
+++ areaPerimeterGreater.java
@@ -0,0 +1,20 @@
+// calculate the area and the perimeter of the rectangle
+package conditional;
+import java.util.Scanner;
+public class areaPerimeterGreater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length");
+        int length = sc.nextInt();
+        System.out.println("Enter the breadth");
+        int breadth = sc.nextInt();
+        int area = length * breadth;
+        int perimeter = 2 * (length * breadth);
+        if(area>perimeter){
+            System.out.println("Area is greater than the perimeter");
+        }else{
+            System.out.println("Perimeter is greater than the area");
+        }
+
+    }
+}
