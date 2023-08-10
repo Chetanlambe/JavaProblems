2bbbf0dc94a1a933adaec83e42b13780f93f1c48
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-10 19:04:04 +0530

    printTenTime
    print the helllo world

---
+++ printTenTime.java
@@ -0,0 +1,10 @@
+package loops;
+// Print 10 time Hello world java
+public class printTenTime {
+    public static void main(String[] args) {
+        int i;
+        for (i=1;i<=10;i++){
+            System.out.println("Hello world java");
+        }
+    }
+}
