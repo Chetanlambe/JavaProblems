e379ed11ccb6d172c35ce733f8ee950e75d790a4
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-11 09:48:42 +0530

    table of nineteen
    print the table of nineteen

---
+++ tableOfNineteen.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class tableOfNineteen {
+    public static void main(String[] args) {
+        int n = 19;
+        for(int i=1;i<=10;i++){
+            System.out.println(n*i);
+        }
+    }
+}
