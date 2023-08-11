ac7ba1d58bd359e0d21b8a9341e4f48570777b71
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-11 09:45:20 +0530

    odd number
    print the odd number

---
+++ oddNumber.java
@@ -0,0 +1,17 @@
+package loops;
+import java.util.Scanner;
+public class oddNumber {
+    public static void main(String[] args) {
+        for (int i=1;i<=100;i++){
+            if(i%2 != 0)
+                System.out.println(i);
+        }
+//        upto n number print the odd number
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for (int i=1;i<=n;i++) {
+//            if (i % 2 != 0)
+//                System.out.println(i);
+//        }
+    }
+}
