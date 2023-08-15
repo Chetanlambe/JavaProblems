7bca41a6c62cbd7ec577e17eb77b09afb2d24a15
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-15 18:17:31 +0530

    print the ascii value
    print the ACSCII value

---
+++ asciiValue.java
@@ -0,0 +1,11 @@
+// Print the ASCII value of char
+
+package loops;
+public class asciiValue {
+    public static void main(String[] args) {
+        for (int i=65;i<=90;i++){
+            char ch = (char)i;
+            System.out.println(ch + " " + i);
+        }
+    }
+}
